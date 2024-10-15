f82909d2b75df43088ac7f97691a26a89e24b87d
Dread Master <67399960+FireEmblem3h@users.noreply.github.com>
2024-10-14 20:05:49 -0600

    Add files via upload

---
+++ Workshop 1/Workshop 1/Workshop 1.cpp
@@ -0,0 +1,97 @@
+// Workshop 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	/*//Two define integers
+	const double CONVERT = 0.56;
+	double tempF, tempC;
+
+	//Asking for input from the User
+	cout << "Enter a temperature (F): "; 
+	cin >> tempF;
+
+	//Calculations
+	tempC = (tempF - 32) * CONVERT; 
+
+	//Display product
+	cout << tempF << " degrees Fahrenheit is " << tempC << " degrees Celsius." << endl;
+
+	return 0;
+	*/
+
+	//--------------------------------------------------------------------------------------------------------
+
+	
+	/*int numEgg, carton, leftOver;
+
+	cout << "How many eggs purchaseed? ";
+	cin >> numEgg; 
+	
+	//Calculations using the % to account for remainder 
+	carton = numEgg / 12;
+	leftOver = numEgg % 12;
+
+	cout << "You have " << carton << " cartons with " << leftOver << " eggs left over." << endl;
+
+	return 0; 
+	*/
+
+	//--------------------------------------------------------------------------------------------------------------
+
+	/*double const CARTON = 3.78;
+	double const LITER_COST = 0.38;
+	double const CARTON_PROFIT = 0.27;
+
+	double totCostMilk, totProfit; 
+	int numMilk, numCartons;  
+	 
+	cout << "Total amount of milk produced: ";
+	cin >> numMilk;
+
+	//Calculations
+	numCartons = numMilk / CARTON;
+	totCostMilk = numMilk * LITER_COST; 
+	totProfit = numCartons * CARTON_PROFIT; 
+
+	cout << "Number of cartons needed: " << numCartons << endl;
+	cout << fixed << setprecision(2) << "Production cost: $" << totCostMilk << endl;
+	cout << fixed << setprecision(2) << "Today's profits: $" << totProfit << endl;  
+
+	return 0;
+	*/
+
+	//------------------------------------------------------------------------------------------------------------------------
+
+/*	ifstream inData;
+	 
+
+	int ga, gb, gc, gd; 
+
+	inData.open("");
+
+
+
+
+	inData.close();
+
+	return 0; 
+	*/
+
+	// ---------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+}
+
+
